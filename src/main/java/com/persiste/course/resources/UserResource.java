9a1ae9d78b3f481a7792f89306673dece658eab3
João Vitor <joaovitorsantos103@hotmail.com>
2023-09-03 12:57:04 -0300

    Service layer, component registration

--- src/main/java/com/persiste/course/resources/UserResource.java
+++ src/main/java/com/persiste/course/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.persiste.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.persiste.course.entities.User;
+import com.persiste.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Tutito","tuttio@hotmail.com","71986572","134587");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
